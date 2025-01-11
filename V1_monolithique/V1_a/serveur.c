ea799582ccac8a3bb9dd3c5dbdc58fbac745ed06
Dylan Rakotomahefa <dylan@unistra.fr>
2025-01-11 02:46:41 +0100

    V1.a FAIT

--- V1_monolithique/V1_a/serveur.c
+++ V1_monolithique/V1_a/serveur.c
@@ -18,9 +18,8 @@ int operation_afficher() {
     printf("Listing all games...\n");
     int nombre_de_jeux = 0;
     for (int i = 0; i < MAX_JEUX; i++) {
-        printf("Game %d: %s => incremente nombre_de_jeux\n", i, jeux[i].NomJeu);
         if (jeux[i].NomJeu[0] != '\0') {
-            printf("%s\n", jeux[i].NomJeu);
+            printf("Game %d: %s => incremente nombre_de_jeux\n", i, jeux[i].NomJeu);
             nombre_de_jeux++;
         }
     }
@@ -57,7 +56,7 @@ int operation_supprimer(DemandeOperation *demande) {
     return -1; // Échec
 }
 
-void operation_simuler(DemandeOperation *demande, int pipefd) {
+void operation_simuler(DemandeOperation *demande, int pipefd_write) {
     printf("Simulating game '%s'...\n", demande->NomJeu);
     for (int i = 0; i < MAX_JEUX; i++) {
         if (strcmp(jeux[i].NomJeu, demande->NomJeu) == 0) {
@@ -66,7 +65,7 @@ void operation_simuler(DemandeOperation *demande, int pipefd) {
                 code[j] = '*';
             }
             int gagnant = rand() % 2; sleep(20);
-            write(pipefd, &gagnant, sizeof(int));
+            write(pipefd_write, &gagnant, sizeof(int));
             free(code);
             return;
         }
@@ -95,107 +94,150 @@ void operation_lancer(DemandeOperation *demande) {
     }
 }
 
-int execute_demande(DemandeOperation OP) {
-    printf("Executing request with CodeOp: %d\n", OP.CodeOp);
-
-    if (OP.CodeOp == 1) {
-        return operation_tester(&OP);
-    }
-
-    if (OP.CodeOp == 2) {
-        return operation_afficher(&OP);
-    }
-
-    if (OP.CodeOp == 3) {
-        int pipefd[2];
-        pipe(pipefd);
-        int result = operation_ajouter(&OP, pipefd[PIPE_WRITE]);
-        close(pipefd[PIPE_WRITE]);
-        char *code = malloc(1000 * sizeof(char));
-        read(pipefd[PIPE_READ], code, 1000 * sizeof(char));
-        close(pipefd[PIPE_READ]);
-        for (int i = 0; i < MAX_JEUX; i++) {
-            if (jeux[i].NomJeu[0] == '\0') {
-                strcpy(jeux[i].NomJeu, OP.NomJeu);
-                jeux[i].Code = code;
-                break;
-            }
-        }
-        return result;
-    }
-
-    if (OP.CodeOp == 4) {
-        return operation_supprimer(&OP);
-    }
-
-    if (OP.CodeOp == 5) {
-        int pipefd2[2];
-        pipe(pipefd2);
-        operation_simuler(&OP, pipefd2[PIPE_WRITE]);
-        close(pipefd2[PIPE_WRITE]);
-        int gagnant;
-        read(pipefd2[PIPE_READ], &gagnant, sizeof(int));
-        close(pipefd2[PIPE_READ]);
-        if (gagnant == 0) {
-            printf("Joueur 1 gagne !\n");
-        } else {
-            printf("Joueur 2 gagne !\n");
-        }
-    }
-
-    if (OP.CodeOp == 6) {
-        operation_lancer(&OP);
-    }
-
-    printf("Unknown operation\n");
-    return -1;
-}
-
 void execute_demande_parallel(DemandeOperation OP, pid_t *PIDf, int *nbfils_non_bloquants) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Child process
-        printf("Child process (PID %d) executing request with CodeOp: %d\n", getpid(), OP.CodeOp);
-        if (OP.CodeOp == 3 || OP.CodeOp == 5) {
-            int pipefd[2];
-            pipe(pipefd);
-            if (OP.CodeOp == 3) {
-                operation_ajouter(&OP, pipefd[PIPE_WRITE]);
-            } else if (OP.CodeOp == 5) {
-                operation_simuler(&OP, pipefd[PIPE_WRITE]);
+    pid_t pid;
+    int pipefd[2];
+
+    if (OP.CodeOp == 1 || OP.CodeOp == 2) {
+        // For Tester and Lister, we can delegate to child process
+        pipe(pipefd);
+        pid = fork();
+        if (pid == 0) {
+            // Child process
+            printf("Child process (PID %d) executing request with CodeOp: %d\n", getpid(), OP.CodeOp);
+            close(pipefd[PIPE_READ]); // Close read end in child
+            if (OP.CodeOp == 1) {
+                operation_tester(&OP);
+            } else if (OP.CodeOp == 2) {
+                operation_afficher();
             }
-            close(pipefd[PIPE_WRITE]);
+            close(pipefd[PIPE_WRITE]); // Close write end in child
+            exit(0);
+        } else {
+            // Parent process
+            printf("Parent process (PID %d) forked child process (PID %d)\n", getpid(), pid);
+            close(pipefd[PIPE_WRITE]); // Close write end in parent
             if (OP.flag == 1) {
-                close(pipefd[PIPE_READ]);
+                printf("BLOQUANT , parent (PID %d) attend fin de l'enfant (PID %d)\n", getpid(), pid);
+                waitpid(pid, NULL, 0);
             } else {
-                if (OP.CodeOp == 5) {
-                    int gagnant;
-                    read(pipefd[PIPE_READ], &gagnant, sizeof(int));
-                    if (gagnant == 0) {
-                        printf("Joueur 1 gagne !\n");
-                    } else {
-                        printf("Joueur 2 gagne !\n");
-                    }
-                }
-                close(pipefd[PIPE_READ]);
+                printf("NON BLOQUANT , parent (PID %d) continue son execution\n", getpid());
+
+                PIDf[*nbfils_non_bloquants] = pid;
+                (*nbfils_non_bloquants)++;
             }
-        } else {
-            execute_demande(OP);
         }
-        printf("EXIT Child process (PID %d) who did request with CodeOp: %d\n", getpid(), OP.CodeOp);
-        exit(0);
-    } else if (pid > 0) {
-        // Parent process
-        printf("Parent process (PID %d) forked child process (PID %d)\n", getpid(), pid);
-        if (OP.flag == 1) {
-            waitpid(pid, NULL, 0);
+    } else if (OP.CodeOp == 3) {
+        // For Ajouter, we need to simulate loading and transfer via pipe
+        pipe(pipefd);
+        pid = fork();
+        if (pid == 0) {
+            // Child process
+            printf("Child process (PID %d) executing request with CodeOp: %d\n", getpid(), OP.CodeOp);
+            close(pipefd[PIPE_READ]); // Close read end in child
+            operation_ajouter(&OP, pipefd[PIPE_WRITE]);
+            close(pipefd[PIPE_WRITE]); // Close write end in child
+            exit(0);
         } else {
-            PIDf[*nbfils_non_bloquants] = pid;
-            (*nbfils_non_bloquants)++;
+            // Parent process
+            printf("Parent process (PID %d) forked child process (PID %d)\n", getpid(), pid);
+            close(pipefd[PIPE_WRITE]); // Close write end in parent
+            printf("BLOQUANT , parent (PID %d) attend fin de l'enfant (PID %d)\n", getpid(), pid);
+            waitpid(pid, NULL, 0); // Wait for child process to finish
+            char *code = malloc(1000 * sizeof(char));
+            read(pipefd[PIPE_READ], code, 1000 * sizeof(char));
+            close(pipefd[PIPE_READ]); // Close read end in parent
+            printf("adding Game '%s' in parent process...\n", OP.NomJeu);
+            for (int i = 0; i < MAX_JEUX; i++) {
+                printf("jeux[%d].NomJeu: %s\n", i, jeux[i].NomJeu);
+                if (jeux[i].NomJeu[0] == '\0') {
+                    strcpy(jeux[i].NomJeu, OP.NomJeu);
+                    jeux[i].Code = code;
+                    printf("added Game '%s' in parent process...\n", jeux[i].NomJeu);
+                    break;
+                }
+            }
+            free(code);
+        }
+    } else if (OP.CodeOp == 4) {
+        // For Supprimer, must be done by parent process
+        printf("Parent process (PID %d) executing request with CodeOp: %d\n", getpid(), OP.CodeOp);
+        printf("BLOQUANT , parent (PID %d)\n", getpid());    
+        operation_supprimer(&OP);
+    } else if (OP.CodeOp == 5) {
+        // For Combat, parent transmits code to child via pipe
+        int pipe_parent_to_child[2]; // Pipe 1 : Parent écrit, enfant lit
+        int pipe_child_to_parent[2]; // Pipe 2 : Enfant écrit, parent lit
+
+        pipe(pipe_parent_to_child) ;
+        pipe(pipe_child_to_parent) ;
+        pid = fork();
+        if (pid == 0) {
+            // --- Processus enfant ---
+            close(pipe_parent_to_child[PIPE_WRITE]); // Fermer l'extrémité d'écriture du pipe parent -> enfant
+            close(pipe_child_to_parent[PIPE_READ]); // Fermer l'extrémité de lecture du pipe enfant -> parent
+
+            printf("Child process (PID %d) executing request with CodeOp: %d\n", getpid(), OP.CodeOp);
+            printf(" '%s' code received from parent process , loading it...\n", OP.NomJeu);
+            char *code = malloc(1000 * sizeof(char));
+            read(pipe_parent_to_child[PIPE_READ], code, 1000 * sizeof(char));
+            printf(" '%s' code to loaded in child process...\n", OP.NomJeu);
+
+            operation_simuler(&OP, pipe_child_to_parent[PIPE_WRITE]);
+            close(pipe_child_to_parent[PIPE_WRITE]); // Fermer l'extrémité d'écriture du pipe enfant -> parent
+            close(pipe_parent_to_child[PIPE_READ]); // Fermer l'extrémité de lecture du pipe parent -> enfant
+            exit(0);
+        } else {
+            // --- Processus parent ---
+            close(pipe_parent_to_child[PIPE_READ]); // Fermer l'extrémité de lecture du pipe parent -> enfant
+            close(pipe_child_to_parent[PIPE_WRITE]); // Fermer l'extrémité d'écriture du pipe enfant -> parent
+
+            printf("Parent process (PID %d) forked child process (PID %d)\n", getpid(), pid);
+            printf("Loading '%s' code to child process...\n", OP.NomJeu);
+            char *code = malloc(1000 * sizeof(char));
+            for (int j = 0; j < 1000; j++) {
+                code[j] = '*';
+            }
+            write(pipe_parent_to_child[PIPE_WRITE], code, 1000 * sizeof(char));
+            free(code);
+            printf(" '%s' code sent to child process...\n", OP.NomJeu);
+
+            int gagnant;
+            read(pipe_child_to_parent[PIPE_READ], &gagnant, sizeof(int));
+            close(pipe_child_to_parent[PIPE_READ]); // Fermer l'extrémité de lecture du pipe enfant -> parent
+            close(pipe_parent_to_child[PIPE_WRITE]); // Fermer l'extrémité d'écriture du pipe parent -> enfant
+
+            if (gagnant == 0) {
+                printf("Joueur 1 gagne !\n");
+            } else {
+                printf("Joueur 2 gagne !\n");
+            }
+
+            printf("BLOQUANT , parent (PID %d) attend fin de l'enfant (PID %d)\n", getpid(), pid);
+            waitpid(pid, NULL, 0); // Attendre la fin de l'enfant
+        }
+    }
+    else if (OP.CodeOp == 6) {
+        // For Lancer, can be done by child process
+        pid = fork();
+        if (pid == 0) {
+            // Child process
+            printf("Child process (PID %d) executing request with CodeOp: %d\n", getpid(), OP.CodeOp);
+            operation_lancer(&OP);
+            exit(0);
+        } else {
+            // Parent process
+            printf("Parent process (PID %d) forked child process (PID %d)\n", getpid(), pid);
+            if (OP.flag == 1) {
+                printf("BLOQUANT , parent (PID %d) attend fin de l'enfant (PID %d)\n", getpid(), pid);            
+                waitpid(pid, NULL, 0);
+            } else {
+                PIDf[*nbfils_non_bloquants] = pid;
+                (*nbfils_non_bloquants)++;
+            }
         }
     } else {
-        perror("fork");
-        exit(1);
+        printf("Unknown operation\n");
     }
 }
 
